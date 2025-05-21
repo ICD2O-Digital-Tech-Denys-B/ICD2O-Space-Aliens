9831a410cd5e9c7bd37547e19048df3e454a924d
Denys Bludov <denys.bludov@stu.ocsb.ca>
2025-05-21 14:43:22 +0000

    rst 1 done

---
+++ js/game.js
@@ -0,0 +1,9 @@
+/* global Phaser */ 
+const config = {
+    type: Phaser.AUTO,
+    width: 1920,
+    height: 1080,
+    backgroundColor: f1f1f1
+}
+const game = new Phaser.Game(config)
+console.log(game)
