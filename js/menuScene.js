4eff8a97a30543022d9f96527289c0c95cd29121
Denys Bludov <denys.bludov@stu.ocsb.ca>
2025-05-26 14:22:37 +0000

    rst 7

--- js/menuScene.js
+++ js/menuScene.js
@@ -8,22 +8,37 @@
  */
 class MenuScene extends Phaser.Scene {
     constructor() {
-        super({ key: 'menuScene' });
+        super({ key: 'menuScene' })
+
+        this.menuSceneBackgroundImage = null
+        this.startButton = null
     }
 
 
     init (data) {
-    this.cameras.main.setBackgroundColor("eb3a15");
+    this.cameras.main.setBackgroundColor("ffffff");
     }
 
     preload() {
         console.log('Menu Scene');
+        this.load.image('menuSceneBackground', 'assets/aliens_screen_image2.jpg')
+        this.load.image('startButton', 'assets/start,png')
     }
 
-    create (data) {
+    create(data) {
+        this.menuSceneBackgroundImage = this.add.sprite(0, 0, 'menuSceneBackground')
+        this.menuSceneBackgroundImage.x = 1920 / 2
+        this.menuSceneBackgroundImage.y = 1080 / 2
+        
+        this.startButton = this.add.sprite(1920 / 2, (1080 / 2) + 100, 'startButton')
+        this.startButton.setInteractive({ useHandCursor: true })
+        this.startButton.on('pointerdown', () => this.clickButton())
     }
 
     update (time, delta) { 
+    }
+    clickButton(){
+        this.scene.start('gameScene')
     }
 }
     export default MenuScene
--- js/titleScene.js
+++ js/titleScene.js
@@ -9,12 +9,18 @@
 class TitleScene extends Phaser.Scene {
     constructor() {
         super({ key: 'titleScene' });
-        this.aliens_screen_image = null
+        this.titleSceneBackground = null
+        this.titleSceneText = null
+        this.titleSceneTextStyle = {
+            font: "200px Times",
+            fill: "fde4b9",
+            align: "center",
+        }
     }
 
 
-    init (data) {
-    this.cameras.main.setBackgroundColor("eb3a15");
+    init(data) {
+        this.cameras.main.setBackgroundColor("ffffff");
     }
 
     preload() {
@@ -22,12 +28,21 @@ class TitleScene extends Phaser.Scene {
         this.load.image('titleSceneBackground', './assets/aliens_screen_image.jpg')
     }
     create(data) {
-        this.titleSceneBackground = this.add.sprite(0, 0, 'titleSceneBackground').setScale(3.75)
+        this.titleSceneBackground = this.add
+            .sprite(0, 0, 'titleSceneBackground')
+            .setScale(3.75)
         this.titleSceneBackground.x = 1920 / 2
         this.titleSceneBackground.y = 1080 / 2
+
+        this.titleSceneText = this.add
+            .text(1920 / 2, 1080 / 2 + 350, "Space Aliens", this.titleSceneTextStyle)
+            .setOrigin(0.5)
     }
 
-    update (time, delta) { 
+    update(time, delta) {
+        if (time > 6000) {
+            this.scene.switch('menuScene')
+        }
     }
 }
     export default TitleScene
