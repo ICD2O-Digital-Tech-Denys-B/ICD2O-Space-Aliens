4da4fbbff36fd7a38f4a3842c39c17c693902839
Denys Bludov <denys.bludov@stu.ocsb.ca>
2025-05-26 14:41:19 +0000

    rst8

--- js/gameScene.js
+++ js/gameScene.js
@@ -2,14 +2,25 @@
 class GameScene extends Phaser.Scene {
     constructor() {
         super({ key: 'gameScene' })
+
+        this.background = null
+        this.ship = null
     }
+
     init(data) {
         this.cameras.main.setBackgroundColor('#ffffff')
     }
     preload() {
-      console.log('Game Scene')
+        console.log('Game Scene')
+        
+        this.load.image('starBackground', 'assets/StarBackground.png')
+        this.load.image('ship', 'assets/spaceShip.png')
     }
     create(data) {
+        this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
+        this.background.setOrigin(0, 0)
+        
+        this.ship = this.physics.add.sprite(1920 / 2, 1080 -100, 'ship')
     }
     update(time, delta) {
     }
