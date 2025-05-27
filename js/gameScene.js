37f02610cd997e8583a1ed38eacc61aa4deb1830
Denys Bludov <denys.bludov@stu.ocsb.ca>
2025-05-27 13:57:15 +0000

    rst10

--- js/gameScene.js
+++ js/gameScene.js
@@ -15,17 +15,21 @@ class GameScene extends Phaser.Scene {
         
         this.load.image('starBackground', 'assets/starBackground.png')
         this.load.image('ship', 'assets/spaceShip.png')
+        this.load.image('missile', 'assets/missile.png')
     }
     create(data) {
         this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
         this.background.setOrigin(0, 0)
         
         this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
+
+        this.missileGroup = this.physics.add.group()
     }
     update(time, delta) {
         
         const keyLeftObj = this.input.keyboard.addKey('LEFT')
         const keyRightObj = this.input.keyboard.addKey('RIGHT')
+        const keySpaceObj = this.input.keyboard.addKey('SPACE')
 
         if (keyLeftObj.isDown === true) {
             this.ship.x -= 15
@@ -39,6 +43,15 @@ class GameScene extends Phaser.Scene {
             if (this.ship.x > 1920) {
                 this.ship.x = 1920
             }
+          }
+        if (keySpaceObj.isDown === true) {
+            if (this.fireMissile === false) {
+                this.fireMissile = true
+                const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, 'missile')
+            }
+        }
+        if (keySpaceObj.isUp === true) {
+            this.fireMissile = false
           }
         }
       }
