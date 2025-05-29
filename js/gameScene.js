b842359e073713d74c45dbe411a0dc7f29547c6f
Denys Bludov <denys.bludov@stu.ocsb.ca>
2025-05-29 14:21:58 +0000

    rst15

--- js/gameScene.js
+++ js/gameScene.js
@@ -67,7 +67,7 @@ class GameScene extends Phaser.Scene {
             shipCollide.destroy()
             this.gameOverText = this.add.text(1920 / 2, 1080 / 2, 'Game Over!\nClick to play again.', this.gameOverTextStyle).setOrigin(0.5)
             this.gameOverText.setInteractive({ useHandCursor: true })
-            this.gameOverText.on('pointerdown', () => this.scene.start('gameScene'))
+            this.gameOverText.on('pointerdown', () => this.scene.restart('gameScene'))
         }.bind(this))
     }
     update(time, delta) {
