73cb6a8c68015d7aacfd5f4ebfc8ae0b7bd665b0
Denys Bludov <denys.bludov@stu.ocsb.ca>
2025-05-30 14:27:22 +0000

    rst15

--- js/gameScene.js
+++ js/gameScene.js
@@ -106,14 +106,15 @@ class GameScene extends Phaser.Scene {
                 this.ship.x = 1920
             }
         }
-            if (keySpaceObj.isDown === true && gameEnd === false) {
-                if (this.fireMissile === false) {
-                    this.fireMissile = true
-                    const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, 'missile')
-                    this.missileGroup.add(aNewMissile)
-                    this.sound.play('laser')
-                }
+        if (keySpaceObj.isDown === true && gameEnd === false) {
+            console.log("Space key pressed and gameEnd is false.")    
+            if (this.fireMissile === false) {
+                this.fireMissile = true
+                const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, 'missile')
+                this.missileGroup.add(aNewMissile)
+                this.sound.play('laser')
             }
+        }
         if (keySpaceObj.isUp === true) {
             this.fireMissile = false
         }
